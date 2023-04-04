ab8eaccb1326e65605a6a989ae567115ddf62ae3
Astbrq Egbaria <115001088+Astbrqeg@users.noreply.github.com>
2023-04-04 18:22:39 +0300

    Delete etoile directory

--- etoile/Etoile_BKE/README.old.md
+++
@@ -1,1 +0,0 @@
-# Etoile_BKE
--- etoile/Etoile_BKE/src/Database/connection.js
+++
@@ -1,7 +0,0 @@
-const pg = require("pg");
-const dotenv = require("dotenv");
-dotenv.config();
-const connectionString = process.env.DATABASE_URL;
-const db = new pg.Pool({ connectionString });
-
-module.exports = db;
